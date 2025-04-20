68910b9e7902fef58147520bb0402c475265cdef
lyhottag13 <leonardo@promoevt.com>
2025-04-20 01:37:01 -0700

    Updated PNGs to WEBPs

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const shop = document.getElementById("shop");
 const shopImage = document.getElementById("shopImage");
+
+const IMAGES = ["Open", "Closed"];
+const preloadedImages = {};
+IMAGES.forEach(fileName => {
+    const image = new Image();
+    image.src = `resources/images/${fileName}.webp`;
+    preloadedImages[fileName] = image;
+});
 shop.addEventListener("mouseenter", () => {
-    shopImage.src = "resources/images/Open.png";
+    shopImage.src = preloadedImages["Open"].src;
 });
 shop.addEventListener("mouseleave", () => {
-    shopImage.src = "resources/images/Closed.png";
+    shopImage.src = preloadedImages["Closed"].src;
 })
